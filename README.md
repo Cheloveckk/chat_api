0454a796a2e348c202aa7b57cd24cf4ed9fe87e4
Cheloveckk <mskrabin51@gmail.com>
2026-06-07 19:36:59 +0700

    A counter has been added, and the README.md file has been update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # chat_api
 api for my chat
+
+how to download git and use him and github: 
+    https://yandex.ru/video/preview/7806513683081671816
+for getting piblic ip with localhost.run write in console this command:
+    ssh -R 80:127.0.0.1:8080 nokey@localhost.run
--- pkg/hello/hello.go
+++ pkg/hello/hello.go
@@ -2,11 +2,15 @@ package hello
 
 import (
 	"net/http"
+	"strconv"
 )
+
+var Counter = 0
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("test", "hello")
-	w.Write([]byte("hello"))
+	w.Write([]byte("hello" + strconv.Itoa(Counter)))
+	Counter += 1
 
 }
 func HelloHandle(handler *http.ServeMux) {
